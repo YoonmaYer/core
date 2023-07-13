889248ad997fbfaa88f01d4a45683ba763ff6a57
YoonmaYer <rkddbs0813@gmail.com>
2023-07-13 18:26:22 +0900

    2023.07.13 - 새로운 할인 정책 적용에 따른 코드 리팩터링(SCP, OCP, DIP 원칙 적용), AppConfig 변경(순수 자바 코드 -> 스프링 코드)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,25 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
-    // DIP 위반
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+//  DIP 위반, 구현체와 인터페이스를 모두 의존함.
+//  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//  private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+
+    // 인터페이스에만 의존하도록 코드를 변경
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice)  {
