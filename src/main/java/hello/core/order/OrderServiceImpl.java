81eeb87d00e60dcaeeb1094fc9b8f3063725c263
YoonmaYer <rkddbs0813@gmail.com>
2023-07-24 21:14:17 +0900

    2023.07.24 - 의존관계 자동 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,13 +1,17 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class OrderServiceImpl implements OrderService {
 
 //  DIP 위반, 구현체와 인터페이스를 모두 의존함.
@@ -20,11 +24,13 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    // @RequiredArgsConstructor  롬복 라이브러리를 쓰면 이 코드 생략 가능, final이 붙은 필드를 모아 생성자를 자동으로 생성해줌.
+    @Autowired // 생성자가 하나면 생략 가능
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice)  {
